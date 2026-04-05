4e34ace5efddc6ee7c658451b0d6135fab15b575
cantthinkofanynames <113739547+cantthinkofanynames@users.noreply.github.com>
2026-04-05 07:39:00 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { MinecraftServer } = require('mcping-js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+// ENV VARIABLES (IMPORTANT)
+const SERVER_IP = process.env.SERVER_IP;
+const SERVER_PORT = process.env.SERVER_PORT || 25565;
+const CHANNEL_ID = process.env.CHANNEL_ID;
+
+const server = new MinecraftServer(SERVER_IP, SERVER_PORT);
+
+async function updateServerStatus() {
+  server.ping(3000, async (err, res) => {
+    const channel = await client.channels.fetch(CHANNEL_ID);
+
+    if (!channel) return;
+
+    if (err) {
+      await channel.setName("🔴 Offline");
+    } else {
+      const online = res.players.online;
+      const max = res.players.max;
+
+      await channel.setName(`🟢 ${online}/${max}`);
+    }
+  });
+}
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+
+  updateServerStatus();
+  setInterval(updateServerStatus, 30000);
+});
+
+client.login(process.env.TOKEN);
